045cc84d9795e493e051b5e7353e7403726915a1
Cross-87 <racq_2016@outlook.com>
2023-06-30 13:18:28 -0600

    base updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://Cross-87.github.io/ToDo-App-TS-React"
 })
